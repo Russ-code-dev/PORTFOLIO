1b5b6c98081a1943d04d1655497457410222f1fb
Russ-code-dev <uyahrussel@gmail.com>
2025-10-01 06:52:59 +0100

    Initial commit

---
+++ port.js
@@ -0,0 +1,11 @@
+
+// The code to select all the nav-links
+const navlist = document.querySelector('.nav-link');
+
+nav-links.forEach(link=>{
+    link.addEventListenner('click', fuction(event)){
+        event.preventDefault();  //stop instantjump
+    }
+
+    Get section id from link (remove the )
+})
